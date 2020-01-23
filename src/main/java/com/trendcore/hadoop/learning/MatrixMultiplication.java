2af55d4e2009ab88fa515b61900072e480713b3a
anurag <anurag.botre.202@gmail.com>
2020-01-23 23:30:58 +0530

    Matrix Multiplication Completed.

--- src/main/java/com/trendcore/hadoop/learning/MatrixMultiplication.java
+++ src/main/java/com/trendcore/hadoop/learning/MatrixMultiplication.java
@@ -1,20 +1,23 @@
 package com.trendcore.hadoop.learning;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.ReflectionUtils;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MatrixMultiplication {
@@ -49,12 +52,17 @@ public class MatrixMultiplication {
         }
     }
 
-    class Pair implements WritableComparable<Pair> {
+    public static class Pair implements WritableComparable<Pair> {
 
         int i;
         int j;
 
         public Pair() {
+            i = 0;
+            j = 0;
+        }
+
+        public Pair(int i, int j) {
             this.i = i;
             this.j = j;
         }
@@ -90,10 +98,7 @@ public class MatrixMultiplication {
 
         @Override
         public String toString() {
-            return "Pair{" +
-                    "i=" + i +
-                    ", j=" + j +
-                    '}';
+            return i + " " + j + " ";
         }
     }
 
@@ -107,7 +112,7 @@ public class MatrixMultiplication {
             String line = value.toString();
             String[] tokens = line.split(",");
 
-            IntWritable writableKey = new IntWritable(Integer.parseInt(tokens[0]));
+            IntWritable writableKey = new IntWritable(Integer.parseInt(tokens[1]));
 
             Element element = new Element(0,
                     Integer.parseInt(tokens[0]),
@@ -139,6 +144,66 @@ public class MatrixMultiplication {
         }
     }
 
+    public static class ReducerMxN extends Reducer<IntWritable, Element, Pair, DoubleWritable> {
+
+        @Override
+        protected void reduce(IntWritable key, Iterable<Element> values, Context context) throws IOException, InterruptedException {
+            ArrayList<Element> M = new ArrayList<>();
+            ArrayList<Element> N = new ArrayList<>();
+
+            Configuration conf = context.getConfiguration();
+
+            for (Element element : values) {
+                Element tempElement = ReflectionUtils.newInstance(Element.class, conf);
+                ReflectionUtils.copy(conf, element, tempElement);
+
+                if (tempElement.tag == 0) {
+                    M.add(tempElement);
+                } else if (tempElement.tag == 1) {
+                    N.add(tempElement);
+                }
+            }
+
+            for (int i = 0; i < M.size(); i++) {
+                for (int j = 0; j < N.size(); j++) {
+                    Pair p = new Pair(M.get(i).index, N.get(j).index);
+                    double multiplyOutput = M.get(i).value * N.get(j).value;
+
+                    context.write(p, new DoubleWritable(multiplyOutput));
+                }
+            }
+        }
+    }
+
+    public static class MapMxN extends Mapper<Object, Text, Pair, DoubleWritable> {
+
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            System.out.println(key + " " + value);
+
+            String readLine = value.toString();
+            String[] pairValue = readLine.split(" ");
+
+            Pair pair = new Pair(Integer.parseInt(pairValue[0]), Integer.parseInt(pairValue[1]));
+            DoubleWritable doubleWritable = new DoubleWritable(Double.parseDouble(pairValue[2]));
+            context.write(pair, doubleWritable);
+        }
+    }
+
+    public static class ReduceMxN extends Reducer<Pair, DoubleWritable, Pair, DoubleWritable> {
+
+        @Override
+        protected void reduce(Pair key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+            double d = 0;
+            for (DoubleWritable value : values) {
+                d = d + value.get();
+            }
+            System.out.println(key.i + " " + key.j + " " + d);
+            context.write(key, new DoubleWritable(d));
+        }
+    }
+
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Job job = Job.getInstance();
         job.setJobName("MapIntermediate");
@@ -152,17 +217,43 @@ public class MatrixMultiplication {
         MultipleInputs.addInputPath(job, new Path(m), TextInputFormat.class, MatrixMapperM.class);
         MultipleInputs.addInputPath(job, new Path(n), TextInputFormat.class, MatrixMapperN.class);
 
+        job.setReducerClass(ReducerMxN.class);
         job.setMapOutputKeyClass(IntWritable.class);
         job.setMapOutputValueClass(Element.class);
 
         job.setOutputKeyClass(Pair.class);
+        job.setOutputValueClass(DoubleWritable.class);
 
-        String maxtrixMultiplicationOutputDir = homePath + "/matrix_multiplication_output";
-        deleteDir(maxtrixMultiplicationOutputDir);
-        FileOutputFormat.setOutputPath(job, new Path(maxtrixMultiplicationOutputDir));
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        String matrixMultiplicationIntermediateDir = homePath + "/matrix_multiplication/intermediate";
+        String matrixMultiplicationOutputDir = homePath + "/matrix_multiplication/output";
+
+        deleteDir(matrixMultiplicationIntermediateDir);
+        deleteDir(matrixMultiplicationOutputDir);
+
+        FileOutputFormat.setOutputPath(job, new Path(matrixMultiplicationIntermediateDir));
 
         job.waitForCompletion(true);
 
+        Job job2 = Job.getInstance();
+        job2.setJobName("MapFinalOutput");
+        job2.setJarByClass(MatrixMultiplication.class);
+
+        job2.setMapperClass(MapMxN.class);
+        job2.setReducerClass(ReduceMxN.class);
+
+        job2.setOutputKeyClass(Pair.class);
+        job2.setOutputValueClass(DoubleWritable.class);
+
+        job2.setInputFormatClass(TextInputFormat.class);
+        job2.setOutputFormatClass(TextOutputFormat.class);
+
+
+        FileInputFormat.setInputPaths(job2, new Path(matrixMultiplicationIntermediateDir));
+        FileOutputFormat.setOutputPath(job2, new Path(matrixMultiplicationOutputDir));
+
+        job2.waitForCompletion(true);
     }
 
     private static void deleteDir(String dir) {
